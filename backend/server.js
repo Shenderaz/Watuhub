92cedc55a956a8f16d1688714bb19f8ba272025b
shenderaz <shenderaz@berea.edu>
2025-11-21 00:17:18 -0500

    Fix the backend server and modified the json file

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Test route
 app.get("/", (req, res) => {
-  res.json({ message: "Watuhub API is running" });
+  res.json({ message: "Testing Testing Watuhub" });
 });
 
 app.listen(5000, () => console.log("Backend listening on port 5000"));
